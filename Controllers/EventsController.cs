c35ba18d20cf6a686be8a509335d21e7726b713d
Randy Parker <randallbparker@gmail.com>
2020-11-03 09:58:06 -0600

    EventsController update using a list of hard coded items to work on the view

---
+++ Controllers/EventsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEvents.Controllers
+{
+    public class EventsController : Controller
+    {
+        static private List<string> Events = new List<string>();
+        // GET: /Events
+        [HttpGet]
+        public IActionResult Index()
+        {
+            Events.Add("Grocery Shopping");
+            Events.Add("Traffic Jam");
+            Events.Add("Work");
+
+            ViewBag.events = Events;
+
+            return View();
+        }
+    }
+}
